3799ea39d878de8c79677419ea06293a92787f9a
Fabio Almeida <fabio.h.s.almeida@gmail.com>
2018-02-23 20:14:14 -0300

    Creating missing exception message; Adding Auction Class implementation;

--- com-fhsa-auction/src/main/java/com/fhsa/auction/domain/Auction.java
+++ com-fhsa-auction/src/main/java/com/fhsa/auction/domain/Auction.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fhsa.auction.exception.auction.AuctionRuntimeException;
+import com.fhsa.auction.exception.util.ExceptionMessages;
+
 public class Auction {
 	private List<Bid> bidsList;
 	private String auctionProductName;
@@ -16,9 +19,62 @@ public class Auction {
 	}
 	
 	public void proposeNewBid(Bid bid) {
+		validNewBidPropose(bid);
 		
+		bidsList.add(bid);
 	}
 	
+	private void validNewBidPropose(Bid bid) {
+		if(!bidsList.isEmpty()) {
+			User user = bid.getUser();
+			if(this.getNumberOfBidByUser(user)==5) {
+				throw new AuctionRuntimeException(ExceptionMessages.AUCTION_EXCEPTION_USER_ALREADY_HAS_FIVE_BIDS);
+			} else if(this.getLastBid().getUser().equals(user)) {
+				throw new AuctionRuntimeException(ExceptionMessages.AUCTION_EXCEPTION_USER_PROPOSE_BIDS_IN_A_ROW);
+			}
+		}
+	}
+	
+	private int getNumberOfBidByUser(User user) {
+		int numberOfBids = 0;
+		for(Bid bid : bidsList) {
+			if(bid.getUser().equals(user)) {
+				numberOfBids += 1;
+			}
+		}
+		
+		return numberOfBids;
+	}
+
+	public void doubleBid(User user) {
+		Bid lastBidOfUser = this.getLastBidOfUser(user);
+		
+		if(lastBidOfUser==null) {
+			throw new AuctionRuntimeException(ExceptionMessages.AUCTION_EXCEPTION_DOUBLE_BID_OF_USER_WITHOUT_BIDS);
+		} else {
+			this.proposeNewBid(new Bid(lastBidOfUser.getValue().multiply(BigDecimal.valueOf(2)), user));
+		}
+	}
+
+	public Bid getLastBid() {
+		if(bidsList.isEmpty()) {
+			throw new AuctionRuntimeException(ExceptionMessages.AUCTION_EXCEPTION_AUCTION_IS_EMTPY);
+		} else {
+			return bidsList.get(bidsList.size()-1);
+		}
+	}
+
+	public Bid getLastBidOfUser(User user) {
+		Bid lastBidOfUser = null;
+		for(Bid bid : bidsList) {
+			if(bid.getUser().equals(user)) {
+				lastBidOfUser = bid;
+			}
+		}
+		
+		return lastBidOfUser;
+	}
+
 	public List<Bid> getBidsList() {
 		return bidsList;
 	}
@@ -29,15 +85,5 @@ public class Auction {
 
 	public BigDecimal getBidsValueAverage() {
 		return bidsValueAverage;
-	}
-
-	public void doubleBid(User user) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public Bid getLastBid() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
--- com-fhsa-auction/src/main/java/com/fhsa/auction/exception/util/ExceptionMessages.java
+++ com-fhsa-auction/src/main/java/com/fhsa/auction/exception/util/ExceptionMessages.java
@@ -9,6 +9,7 @@ public class ExceptionMessages {
 	public static String AUCTION_EXCEPTION_USER_ALREADY_HAS_FIVE_BIDS = "Isn't possible for an user to propose more than five bids in the same auction";
 	public static String AUCTION_EXCEPTION_USER_PROPOSE_BIDS_IN_A_ROW = "Isn't possible for an user to propose more than one bid in a row.";
 	public static String AUCTION_EXCEPTION_DOUBLE_BID_OF_USER_WITHOUT_BIDS = "Isn't possible for an user to double his last bid if there isn't a previous bid from this user.";
+	public static String AUCTION_EXCEPTION_AUCTION_IS_EMTPY = "Isn't possible to return the last bid of a empty auction.";
 	
 	public static String AUCTIONEER_EXCEPTION_NONEXISTENT_AUCTION = "Isn't possible to evaluate an nonexistent auction.";
 }
